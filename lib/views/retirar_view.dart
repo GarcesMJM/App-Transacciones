259b4d8cbdcf7f1a79a20bf2241be007f73dfe49
GarcesMJM <89167308+GarcesMJM@users.noreply.github.com>
2023-05-01 21:54:29 -0500

    Codificación de la vista Retirar

---
+++ lib/views/retirar_view.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class RetirarView extends StatelessWidget {
+  const RetirarView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(1, 255, 255, 255),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          const Center(
+            child: Text(
+              "INGRESE EL CÓDIGO PARA RETIRAR",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 200,
+            child: TextFormField(
+              decoration: const InputDecoration(),
+              style: const TextStyle(
+                color: Colors.amber
+              ),
+            ),    
+          ),
+          ElevatedButton(
+            onPressed: (){
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.amber,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(50)
+              )
+            ),
+            child: const Text(
+              "ACEPTAR",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+                fontWeight: FontWeight.bold
+
+              ),
+            )
+          )
+        ],
+      ),
+    );
+  }
+}
