bea4aa5756b91d907fd19e668c19b6dad4393e0a
GarcesMJM <89167308+GarcesMJM@users.noreply.github.com>
2023-05-01 22:51:30 -0500

    Codificación de la vista Consultar bienes

---
+++ lib/views/consultarb_view.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ConsultarbView extends StatelessWidget {
+  const ConsultarbView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Row(
+            children: const[
+              Text(
+                "PROPIEDADES", 
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: const[
+              Text(
+                "Propiedad #", 
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              Text(
+                "Valor", 
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
